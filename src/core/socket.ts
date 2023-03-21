5a34e4955c3c611fb599c94a28bebc26c00249a8
Manannikov Kirill <113590303+themankov@users.noreply.github.com>
2023-03-21 20:34:05 +0300

    Update sockets

--- src/core/socket.ts
+++ src/core/socket.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:7000', {
+const socket = io('https://chat-app-backend-7dsr.onrender.com', {
   withCredentials: true,
 });
 
